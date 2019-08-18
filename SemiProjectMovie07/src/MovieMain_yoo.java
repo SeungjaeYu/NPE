ea3bf2558bf4155617b5cafd7f298b578a27f608
유지인 <esther.yoo329@gmail.com>
2019-08-18 16:17:35 +0900

    19.08.18

--- SemiProjectMovie07/src/MovieMain_yoo.java
+++ SemiProjectMovie07/src/MovieMain_yoo.java
@@ -2,7 +2,6 @@ import ui.MainUI_yoo;
 
 public class MovieMain_yoo {
 	public static void main(String[] args) {
-		System.out.println("yoo sratr");
 		new MainUI_yoo().service();
 	}
 }
--- SemiProjectMovie07/src/ui/MainUI_yoo.java
+++ SemiProjectMovie07/src/ui/MainUI_yoo.java
@@ -1,6 +1,5 @@
 package ui;
 
-
 import util.CommUtil;
 
 public class MainUI_yoo {
