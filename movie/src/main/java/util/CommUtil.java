4e939345214ccef76c6e6c8c508ef89c61443ddc
BoreumSong <sngbrm99@gmail.com>
2019-08-21 19:28:15 +0900

    2019.08.21 비밀번호 암호화

--- movie/src/main/java/util/CommUtil.java
+++ movie/src/main/java/util/CommUtil.java
@@ -8,49 +8,49 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
-
-
+import java.security.MessageDigest;
 
 /**
- *  이 클래스는 공통적으로 사용하는 모듈을 모아 놓은 클래스이다.
+ * 이 클래스는 공통적으로 사용하는 모듈을 모아 놓은 클래스이다.
  *
  * @since 2019-07-23
  * @author sj Yu
  *
  */
 
-
 public class CommUtil {
 	public SimpleDateFormat loginSdf = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");
 	private static Scanner sc = new Scanner(System.in);
-	
+
 	private static final String DATE_FORMAT = "yyyy-MM-dd";
 	private static final String DATE_FORMAT_TIME = "yyyy-MM-dd HH:mm:ss";
-	
+
 	/**
 	 * 년/월/일 시:분:초를 구하여 문자열로 반환한다.
+	 * 
 	 * @return
 	 */
 	public static String getDate() {
 		return getDate(DATE_FORMAT);
 	}
-	
-	/**
-	 * formatString를 구하여 문자열로 반환한다.
-	 * @return
-	 */
-	
-	public static String getDate(String formatString) {
-		Calendar c = Calendar.getInstance();
-        c.setTime(new Date());
-        DateFormat df = new SimpleDateFormat(formatString);
-        return df.format(c.getTime());
-	}
-	
 
 	/**
-	 * 	parseDate를 구하여 문자열로 반환한다.
-	 * 	@return
+	 * formatString를 구하여 문자열로 반환한다.
+	 * 
+	 * @return
+	 */
+
+	public static String getDate(String formatString) {
+		Calendar c = Calendar.getInstance();
+		c.setTime(new Date());
+		DateFormat df = new SimpleDateFormat(formatString);
+		return df.format(c.getTime());
+	}
+
+	/**
+	 * parseDate를 구하여 문자열로 반환한다.
+	 * 
+	 * @return
 	 */
 	public static String getDate(Date parseDate) {
 		Calendar c = Calendar.getInstance();
@@ -61,7 +61,7 @@ public class CommUtil {
 
 	/**
 	 * 
-	 *  스캐너를 입력받아 정수형을 리턴해준다.
+	 * 스캐너를 입력받아 정수형을 리턴해준다.
 	 * 
 	 * @param val
 	 * @return
@@ -74,12 +74,13 @@ public class CommUtil {
 			} catch (Exception e) {
 				System.out.println("잘못된 값을 입력하셨습니다. 제대로 된 문자를 넣어주세요");
 			}
-			
+
 		}
 	}
+
 	/**
 	 * 
-	 *  스캐너를 입력받아 문자열을 리턴해준다.
+	 * 스캐너를 입력받아 문자열을 리턴해준다.
 	 * 
 	 * @param val
 	 * @return
@@ -92,25 +93,24 @@ public class CommUtil {
 			} catch (Exception e) {
 				System.out.println("잘못된 값을 입력하셨습니다. 제대로 된 숫자를 넣어주세요");
 			}
-			
+
 		}
 	}
-	
 
 	/**
 	 * 
-	 *  좌석 행을 입력받아 문자로 반환해준다.
+	 * 좌석 행을 입력받아 문자로 반환해준다.
 	 * 
 	 * @param reservCol
 	 * @return
 	 */
 	public static char getReservRow(int reservRow) {
-		return (char)('A' + reservRow);
+		return (char) ('A' + reservRow);
 	}
-	
+
 	/**
 	 * 
-	 *  좌석 열을 입력받아 좌석자리를 반환해준다.
+	 * 좌석 열을 입력받아 좌석자리를 반환해준다.
 	 * 
 	 * @param reservRow
 	 * @return
@@ -118,9 +118,10 @@ public class CommUtil {
 	public static int getReservCol(int reservCol) {
 		return reservCol + 1;
 	}
-	
+
 	/**
-	 *  좌석 행을 입력받아 숫자로 반환해준다.
+	 * 좌석 행을 입력받아 숫자로 반환해준다.
+	 * 
 	 * @param reservCol
 	 * @return
 	 */
@@ -134,9 +135,10 @@ public class CommUtil {
 		}
 		return (char) (parseReservRow - 'A');
 	}
-	
+
 	/**
-	 *  좌석 열을 입력받아 숫자로 반환해준다.
+	 * 좌석 열을 입력받아 숫자로 반환해준다.
+	 * 
 	 * @param reservRow
 	 * @return
 	 */
@@ -144,8 +146,8 @@ public class CommUtil {
 		return reservCol - 1;
 	}
 
-	
-	
-	public static String[] movieTimeList = new String[] {"09:00","11:30", "13:10", "15:20", "17:00", "19:10", "21:30", "23:00", "24:40" };
+	public static String[] movieTimeList = new String[] { "09:00", "11:30", "13:10", "15:20", "17:00", "19:10", "21:30",
+			"23:00", "24:40" };
+
 	
 }
